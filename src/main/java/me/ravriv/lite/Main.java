9aa8cc3737126b02262a3966d907faad9d5b3aad
Raven Rivera <125807666+ravriv@users.noreply.github.com>
2026-06-17 04:45:42 +0800

    Update Main.java

--- src/main/java/me/ravriv/lite/Main.java
+++ src/main/java/me/ravriv/lite/Main.java
@@ -1,17 +1,34 @@
 package me.ravriv.lite;
 
-import me.ravriv.lite.handlers.CullThread;
+import me.ravriv.lite.handlers.ParticleCullingHandler;
+import me.ravriv.lite.utils.ClassTransformer;
+import me.ravriv.lite.utils.ReloadListener;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.IReloadableResourceManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "lglt", name = "Legacy Lite", version = "1.0")
 public class Main {
-    public static CullThread cullThread;
+    public static ParticleCullingHandler cullThread;
+
+    @EventHandler
+    public void onInit(FMLInitializationEvent event) {
+        IReloadableResourceManager resourceManager = (IReloadableResourceManager) Minecraft.getMinecraft().getResourceManager();
+        resourceManager.registerReloadListener(new ReloadListener());
+    }
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        ClassTransformer.initOptiFine();
+    }
 
     @EventHandler
     public void onLoadComplete(FMLLoadCompleteEvent event) {
-        cullThread = new CullThread();
+        cullThread = new ParticleCullingHandler();
         cullThread.start();
     }
 }
